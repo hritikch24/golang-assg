d8a7100da66f2f757af571ef6fbd4323d3bd3696
Ubuntu <ubuntu@ip-172-31-30-129.ec2.internal>
2022-08-24 12:45:48 +0000

    Files added

--- linux/automation.sh
+++ linux/automation.sh
@@ -1,6 +1,6 @@
 !#bin/bash
 timestamp=$(date +%Y%m%d%H%M%S)  
-sudo docker build -t assignment:${timestamp} -f ./../dockerize/Dockerfile
+sudo docker build -t assignment:${timestamp} ./../dockerize/
 cp script.yaml new-app.yaml
 sed -i 's/- image.* */- image: assignment:'${timestamp}'/g' ./new-app.yml
 kubectl diff -f new-app.yml
